868098cae54877efbccad5d5ff3f3722f3e24a94
Swapnil Gamit <gamitswapnil7@gmail.com>
2025-04-08 23:19:49 +0530

    2Qsorting&!Arr

--- 14.Arrays/rotatedSearch.java
+++ 14.Arrays/rotatedSearch.java
@@ -1,7 +1,31 @@
 public class rotatedSearch {
 
-    public static int search(int nums[], int key) {
-      
+    public static int search(int nums[], int target) {
+        int start = 0 ; 
+        int end = nums.length-1;
+
+        while(start <= end ){
+
+            int mid = (start +end)/2;
+            if(nums[mid]==target) return mid;
+            // now identify tha which part is sortrd is it left or right 
+            
+            if(nums[start] <= nums[mid]){ // for leftpart if it is sorted must follow this  start < mid  else 100% right part sorted 
+                // now for sorted part's search target in left part 
+
+               if(nums[start] <= target && target <= nums[mid]){
+                  end=mid-1;
+               }else start=mid+1;
+            } 
+            // rightpart
+            else{
+                if( nums[mid] <= target &&  target<= nums[end]){
+                    start= mid+1;
+                }else{
+                    end = mid -1;
+                }
+            }
+        }
         return -1;
     }
 
---
+++ 15.BasicSortingAlgorithms/basicSortingalgo.java
@@ -0,0 +1,45 @@
+public class basicSortingalgo {
+
+    public static void bubblesort(int arr[]) {
+
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            // swap
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 2, 1 };
+        // bubblesort(arr);
+        selectionSort(arr);
+        printArr(arr);
+    }
+
+}
